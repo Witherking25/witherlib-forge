3d45d22fe17429a8ee3052e46c8acef55b7fb711
Witherking25 <aaronkoonce25@gmail.com>
2021-08-04 21:46:05 -0500

    code cleanup

--- src/main/java/com/withertech/witherlib/WitherLib.java
+++ src/main/java/com/withertech/witherlib/WitherLib.java
@@ -27,26 +27,18 @@ import net.minecraft.item.Item;
 import net.minecraft.item.ItemGroup;
 import net.minecraft.item.ItemStack;
 import net.minecraftforge.common.MinecraftForge;
-import net.minecraftforge.eventbus.api.SubscribeEvent;
 import net.minecraftforge.fml.common.Mod;
-import net.minecraftforge.fml.event.lifecycle.FMLClientSetupEvent;
-import net.minecraftforge.fml.event.lifecycle.FMLCommonSetupEvent;
-import net.minecraftforge.fml.event.lifecycle.InterModEnqueueEvent;
-import net.minecraftforge.fml.event.lifecycle.InterModProcessEvent;
-import net.minecraftforge.fml.event.server.FMLServerStartingEvent;
 import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
 import javax.annotation.Nonnull;
 
-// The value here should match an entry in the META-INF/mods.toml file
 @Mod(WitherLib.MODID)
 public class WitherLib
 {
     public static final String MODID = "witherlib";
-    // Directly reference a log4j logger.
-    private static final Logger LOGGER = LogManager.getLogger();
+    public static final Logger LOGGER = LogManager.getLogger();
 
 
     public static ItemGroup TEST_TAB;
@@ -56,16 +48,6 @@ public class WitherLib
 
     public WitherLib()
     {
-        // Register the setup method for modloading
-        FMLJavaModLoadingContext.get().getModEventBus().addListener(this::setup);
-        // Register the enqueueIMC method for modloading
-        FMLJavaModLoadingContext.get().getModEventBus().addListener(this::enqueueIMC);
-        // Register the processIMC method for modloading
-        FMLJavaModLoadingContext.get().getModEventBus().addListener(this::processIMC);
-        // Register the doClientStuff method for modloading
-        FMLJavaModLoadingContext.get().getModEventBus().addListener(this::doClientStuff);
-
-        // Register ourselves for server and other game events we are interested in
         MinecraftForge.EVENT_BUS.register(this);
         if (System.getenv().containsKey("witherlib.dev"))
         {
@@ -78,47 +60,15 @@ public class WitherLib
                     return new ItemStack(ITEMS.get("test_item").get());
                 }
             };
+            LOGGER.debug("Registering Blocks");
             BLOCKS = BuilderRegistry.Builder.create(WitherLib.MODID, Block.class)
                     .add("test_block", () -> new Block(AbstractBlock.Properties.create(Material.ROCK)))
                     .build(FMLJavaModLoadingContext.get().getModEventBus());
+            LOGGER.debug("Registering Items");
             ITEMS = BuilderRegistry.Builder.create(WitherLib.MODID, Item.class)
                     .add("test_item", () -> new Item(new Item.Properties().group(TEST_TAB)))
                     .add("test_block_item", () -> new BlockItem(BLOCKS.get("test_block").get(), new Item.Properties().group(TEST_TAB)))
                     .build(FMLJavaModLoadingContext.get().getModEventBus());
         }
-    }
-
-    private void setup(final FMLCommonSetupEvent event)
-    {
-
-    }
-
-    private void doClientStuff(final FMLClientSetupEvent event)
-    {
-
-    }
-
-    private void enqueueIMC(final InterModEnqueueEvent event)
-    {
-        // some example code to dispatch IMC to another mod
-//        InterModComms.sendTo("witherlib", "helloworld", () ->
-//        {
-//            return "Hello world";
-//        });
-    }
-
-    private void processIMC(final InterModProcessEvent event)
-    {
-//        // some example code to receive and process InterModComms from other mods
-//        LOGGER.info("Got IMC {}", event.getIMCStream().
-//                map(m -> m.getMessageSupplier().get()).
-//                collect(Collectors.toList()));
-    }
-
-    // You can use SubscribeEvent and let the Event Bus discover methods to call
-    @SubscribeEvent
-    public void onServerStarting(FMLServerStartingEvent event)
-    {
-
     }
 }
